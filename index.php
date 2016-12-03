d7425c402fe45c1c858e241d7e354b6b41ff3231
wei.shen <wei.shen@vipdiskon.com>
2016-12-03 12:20:57 +0800

    cn font

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: wei.shen
- * Date: 2016/12/2
- * Time: 16:16
- */
-require 'vendor/autoload.php';
-date_default_timezone_set('UTC');
-if (!session_id()) {
-    session_start();
-}
-$fb = new Facebook\Facebook([
-    'app_id' => '255732791508763', // Replace {app-id} with your app id
-    'app_secret' => 'a7397a042bf00d1f31dc8315b0a6afec',
-    'default_graph_version' => 'v2.2',
-]);
-
-$helper = $fb->getRedirectLoginHelper();
-
-$permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('https://ipsite.site/ipsite/login-callback.php', $permissions);
-
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <!--The font :http://www.youziku.com/webfont/index/45803-->
+    <script type="text/javascript" src="https://cdn.webfont.youziku.com/wwwroot/js/wf/youziku.api.min.js"></script>
+</head>
+<body>
+<h2>中文美</h2>
+<script type="text/javascript">
+    $youziku.load("body", "d210a6e7bb684cc1bdc424c50a53041d", "LiDeBiao-Xing3");
+    /*$youziku.load("#id1,.class1,h1", "d210a6e7bb684cc1bdc424c50a53041d", "LiDeBiao-Xing3");*/
+    /*．．．*/
+    $youziku.draw();
+</script>
+</body>
+</html>
