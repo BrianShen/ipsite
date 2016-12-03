f6b18807c915cc9921c67d2b14506f6432211115
wei.shen <wei.shen@vipdiskon.com>
2016-12-03 18:09:42 +0800

    dd

--- index.php
+++ index.php
@@ -4,16 +4,26 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
+    <link rel="shortcut icon" href="lock.ico">
+    <link type="text/css" rel="stylesheet" href="app.css">
     <!--The font :http://www.youziku.com/webfont/index/45803-->
-    <script type="text/javascript" src="https://cdn.webfont.youziku.com/wwwroot/js/wf/youziku.api.min.js"></script>
+<!--    <script type="text/javascript" src="https://cdn.webfont.youziku.com/wwwroot/js/wf/youziku.api.min.js"></script>-->
 </head>
 <body>
-<h2>中文美</h2>
-<script type="text/javascript">
-    $youziku.load("body", "d210a6e7bb684cc1bdc424c50a53041d", "LiDeBiao-Xing3");
-    /*$youziku.load("#id1,.class1,h1", "d210a6e7bb684cc1bdc424c50a53041d", "LiDeBiao-Xing3");*/
-    /*．．．*/
-    $youziku.draw();
-</script>
+<div id="container">
+    <div class="enter">
+        <a href="http://www.baidu.com">
+
+            <img src="lock-main.png">
+        </a>
+        <div class="check-form">
+            <input type="password" >
+        </div>
+    </div>
+</div>
+<!--<script type="text/javascript">-->
+<!--    //$youziku.load("body", "d210a6e7bb684cc1bdc424c50a53041d", "LiDeBiao-Xing3");-->
+<!--    $youziku.draw();-->
+<!--</script>-->
 </body>
 </html>
